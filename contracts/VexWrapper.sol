6bb83065ab4094b75526ce9a579ed9e6119c3aeb
fede-rodes <federodes@gmail.com>
2024-06-18 18:33:30 +1200

    bumping project pragme to 0_8_20

--- contracts/VexWrapper.sol
+++ contracts/VexWrapper.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity =0.6.6;
+pragma solidity =0.8.20;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IVexchangeV2Router02 } from "./interfaces/IVexchangeV2Router02.sol";
 
 /**
--- contracts/vexchange/vexchange-lib/contracts/test/BabylonianEchidnaTest.sol
+++ contracts/vexchange/vexchange-lib/contracts/test/BabylonianEchidnaTest.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity >=0.4.0;
+pragma solidity >=0.4.0 <0.7.0;
+// pragma solidity >=0.4.0;
 
 import '../libraries/Babylonian.sol';
 
